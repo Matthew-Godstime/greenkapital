4c1f743925fb3fe0dd16f0bfaa42df6ffb99bcb4
Matthew God'stime <matthewgodstime8585@gmail.com>
2022-06-24 02:01:02 +0100

    New Slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { AboutComponent } from './about/about.component';
 import { ElementAnimationDirective } from './element-animation.directive';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
+import { ImageCropperModule } from 'ngx-image-cropper';
+import { NgxUsefulSwiperModule } from 'ngx-useful-swiper';
+import { NgImageSliderModule } from 'ng-image-slider';
 
 const routs: Routes = [
   { path: 'home-page', component: HomePageComponent },
@@ -22,6 +27,10 @@ const routs: Routes = [
   ],
   imports: [
     BrowserModule,
+    SlickCarouselModule,
+    ImageCropperModule,
+    NgxUsefulSwiperModule,
+    NgImageSliderModule,
     RouterModule.forRoot(routs),
   ],
   providers: [],
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,29 +1,82 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, NavigationStart } from '@angular/router';
-import { Observable } from 'rxjs';
-import { filter } from 'rxjs/operators';
+import Swiper, { SwiperOptions } from 'swiper';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css', '../../assets/flickicity.css']
 })
 export class HomePageComponent implements OnInit {
 
-  navStart!: Observable<NavigationStart>;
-  amount: number = 0.00
-  constructor(private router: Router) {
-    // Create a new Observable that publishes only the NavigationStart event
-    this.navStart = router.events.pipe(
-      filter(evt => evt instanceof NavigationStart)
-    ) as Observable<NavigationStart>;
-  }
+
+  Images: Array<any> = [
+    {
+      src: '../../assets/images/7.jpg',
+      alt: 'Image 1',
+    }, {
+      src: '../../assets/images/1-1024x683-1.jpg',
+      alt: 'Image 2'
+    }
+  ]
+
+  config: SwiperOptions = {
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    autoplay: true,
+    grabCursor: true,
+  };
+
+  //---------------------------------------
+  imgCollection: Array<object> = [
+    {
+      image: '../../assets/images/R-64.webp',
+      thumbImage: '../../assets/images/R-64.webp',
+      alt: 'Image 1',
+      title: 'Image 1'
+    }, {
+      image: '../../assets/images/inverter.webp',
+      thumbImage: '../../assets/images/inverter.webp',
+      title: 'Image 2',
+      alt: 'Image 2'
+    }, {
+      image: '../../assets/images/batteries.webp',
+      thumbImage: '../../assets/images/batteries.webp',
+      title: 'Image 3',
+      alt: 'Image 3'
+    }, {
+      image: '../../assets/images/61a2c8d2bf422988703daac42785e13d_medium.jpg',
+      thumbImage: '../../assets/images/61a2c8d2bf422988703daac42785e13d_medium.jpg',
+      title: 'Image 4',
+      alt: 'Image 4'
+    }, {
+      image: '../../assets/images/R-2022-02-14T072435.056.webp',
+      thumbImage: '../../assets/images/R-2022-02-14T072435.056.webp',
+      title: 'Image 5',
+      alt: 'Image 5'
+    }, {
+      image: '../../assets/images/R-2022-03-04T003322.388.webp',
+      thumbImage: '../../assets/images/R-2022-03-04T003322.388.webp',
+      title: 'Image 6',
+      alt: 'Image 6'
+    },
+    {
+      image: '../../assets/images/20w-solar-light-1.webp',
+      thumbImage: '../../assets/images/20w-solar-light-1.webp',
+      title: 'Image 6',
+      alt: 'Image 6'
+    },
+  ];
+  
+  constructor() { }
 
   ngOnInit(): void {
-    this.navStart.subscribe(() => {
-      console.log('Navigation Started!');
-      
-    });
+    
   }
 
 }
